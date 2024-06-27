1e602e123465bc355560125760217704f3ea7607
Karan Vallakati <karan.vallakati@colruytgroup.com>
2024-06-27 16:46:25 +0530

    comments

--- src/main/java/com/example/bookmanagement/BookManagementApplication.java
+++ src/main/java/com/example/bookmanagement/BookManagementApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BookManagementApplication {
-
+// todo : pls add gitignore
 	public static void main(String[] args) {
 		SpringApplication.run(BookManagementApplication.class, args);
 	}
